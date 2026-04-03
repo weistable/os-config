7dc288e2555a3a11e1a84d7b88069a0905a63e95
weistable <136341324+weistable@users.noreply.github.com>
2026-04-03 13:08:05 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,13 +1,6 @@
 {
   description = ''
-    For questions just DM me on X: https://twitter.com/@m3tam3re
-    There is also some NIXOS content on my YT channel: https://www.youtube.com/@m3tam3re
-
-    One of the best ways to learn NIXOS is to read other peoples configurations. I have personally learned a lot from Gabriel Fontes configs:
-    https://github.com/Misterio77/nix-starter-configs
-    https://github.com/Misterio77/nix-config
-
-    Please also check out the starter configs mentioned above.
+    nixos config y540
   '';
 
   inputs = {
