d319f1dd6aed3f543e80eca27ccc6b39e2529c2e
weistable <136341324+weistable@users.noreply.github.com>
2026-04-03 13:33:54 +0800

    Update filesystems.nix

--- hosts/gp2e-pc/filesystems.nix
+++ hosts/gp2e-pc/filesystems.nix
@@ -10,39 +10,39 @@
 
   # ── ESP ───────────────────────────────────────────────────────────────
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/ACB1-676B";
+    device = "/dev/disk/by-uuid/E795-3170";
     fsType = "vfat";
     options = [ "fmask=0177" "dmask=0077" "noexec" "nosuid" "nodev" ];
   };
 
   # ── Btrfs subvolumes ──────────────────────────────────────────────────
   fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/944427b2-861e-464d-af3c-e8da014483bd";
+    device = "/dev/disk/by-uuid/08e7f0f7-eef8-4c73-a51e-692467cb83f6";
     fsType = "btrfs";
     options = [ "subvol=@nix" "compress-force=zstd:1" "noatime" ];
   };
 
   fileSystems."/persistent" = {
-    device = "/dev/disk/by-uuid/944427b2-861e-464d-af3c-e8da014483bd";
+    device = "/dev/disk/by-uuid/08e7f0f7-eef8-4c73-a51e-692467cb83f6";
     fsType = "btrfs";
     options = [ "subvol=@persistent" "compress-force=zstd:1" ];
     neededForBoot = true;
   };
 
   fileSystems."/snapshots" = {
-    device = "/dev/disk/by-uuid/944427b2-861e-464d-af3c-e8da014483bd";
+    device = "/dev/disk/by-uuid/08e7f0f7-eef8-4c73-a51e-692467cb83f6";
     fsType = "btrfs";
     options = [ "subvol=@snapshots" "compress-force=zstd:1" ];
   };
 
   fileSystems."/tmp" = {
-    device = "/dev/disk/by-uuid/944427b2-861e-464d-af3c-e8da014483bd";
+    device = "/dev/disk/by-uuid/08e7f0f7-eef8-4c73-a51e-692467cb83f6";
     fsType = "btrfs";
     options = [ "subvol=@tmp" "compress-force=zstd:1" ];
   };
 
   fileSystems."/btr_pool" = {
-    device = "/dev/disk/by-uuid/944427b2-861e-464d-af3c-e8da014483bd";
+    device = "/dev/disk/by-uuid/08e7f0f7-eef8-4c73-a51e-692467cb83f6";
     fsType = "btrfs";
     options = [ "subvolid=5" ];
   };
